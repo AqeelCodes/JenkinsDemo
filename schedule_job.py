2afebcda35cbf9e8893295facf103655651175a2
Kartik Tanwar <53700012+AqeelCodes@users.noreply.github.com>
2021-04-21 23:11:00 +0530

    Create schedule_job.py

---
+++ schedule_job.py
@@ -0,0 +1,10 @@
+import jenkins
+import os
+
+
+server = jenkins.Jenkins('http://localhost:8080', username='aqeel', password='1150f04cbd7f975e0ace403088389dc7af')
+last_build_number = server.get_job_info('FAKT_Job')['builds'][0]['number']
+build_info = server.get_build_info('FAKT_Job', last_build_number)['actions'][0]['parameters'][0]['value']
+jobs_address = '/Users/aqeel/fakt_remote/' + str(build_info)
+for filename in os.listdir(jobs_address):
+    print(filename)
